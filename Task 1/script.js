54a8e89832a12fca571715fd15e4d9967fe66c32
Vasilij S <vasilij.sam@gmail.com>
2022-04-28 19:23:44 +0300

    Task 1 complete

--- Task 1/script.js
+++ Task 1/script.js
@@ -8,3 +8,21 @@ pamatyti jo pateikto svorio kovertavimą į:
 Pastaba: atvaizdavimas turi būti matomas pateikus formą ir pateikiamas
 <div id="output"></div> viduje, bei turi turėti bent minimalų stilių;
 ------------------------------------------------------------------- */
+const outputEl = document.getElementById('output');
+document.getElementById('submit-btn').addEventListener('click', (event) => {
+	event.preventDefault();
+	outputEl.textContent = '';
+	const inputValue = document.getElementById('search').value;
+	const lbValue = inputValue * 2.2046;
+	const gValue = inputValue / 0.001;
+	const ozValue = inputValue * 35.274;
+	const divEl = document.createElement('div');
+	const lb = document.createElement('h3');
+	const g = document.createElement('h3');
+	const oz = document.createElement('h3');
+	lb.textContent = `Mase svarais: ${lbValue} lb.`;
+	g.textContent = `Mase gramais: ${gValue} g.`;
+	oz.textContent = `Mase uncijom: ${ozValue} oz.`;
+	divEl.append(lb, g, oz);
+	outputEl.append(divEl);
+});
