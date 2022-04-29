99ca0e9d5142cdaf650548040da0bf597566d792
Vasilij S <vasilij.sam@gmail.com>
2022-04-29 11:15:15 +0300

    Task 3 done

--- Task 3/script.js
+++ Task 3/script.js
@@ -11,3 +11,31 @@ Pastaba: Informacija apie user'į (jo kortelė) bei turi turėti bent minimalų 
 -------------------------------------------------------------------------- */
 
 const ENDPOINT = 'https://api.github.com/users';
+const outputEl = document.getElementById('output');
+
+async function onFetch() {
+	try {
+		const response = await fetch(ENDPOINT);
+		const data = await response.json();
+		return data;
+	} catch (err) {
+		console.log(err);
+	}
+}
+const displayUser = (users) => {
+	users.forEach((user) => {
+		const divEl = document.createElement('div');
+		const loginEl = document.createElement('h3');
+		const avatarEl = document.createElement('img');
+		loginEl.textContent = user.login;
+		avatarEl.src = user.avatar_url;
+		divEl.append(loginEl, avatarEl);
+		outputEl.append(divEl);
+	});
+};
+
+document.getElementById('btn').addEventListener('click', async () => {
+	outputEl.textContent = '';
+	const user = await onFetch();
+	displayUser(user);
+});
