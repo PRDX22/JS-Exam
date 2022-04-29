8ea8da329a3ffc8eced043dda93d5aa3ac528ce4
Vasilij S <vasilij.sam@gmail.com>
2022-04-29 19:15:34 +0300

    Task 4 done

--- Task 4/script.js
+++ Task 4/script.js
@@ -9,4 +9,36 @@ Pastaba: Informacija apie automobilį (brand) (jo kortelė) bei turi turėti
 bent minimalų stilių;
 -------------------------------------------------------------------------- */
 
-const ENDPOINT = 'cars.json';
+const ENDPOINT = './cars.json';
+const outputEl = document.getElementById('output');
+
+async function onFetch() {
+	try {
+		const response = await fetch(ENDPOINT);
+		const data = await response.json();
+		console.log(data);
+		return data;
+	} catch (err) {
+		console.log(err);
+	}
+}
+const displayCars = (cars) => {
+	cars.forEach((car) => {
+		const brandDiv = document.createElement('div');
+		const brandEl = document.createElement('h3');
+		const modelsEl = document.createElement('div');
+		brandEl.textContent = car.brand;
+		car.models.forEach((model) => {
+			const modelEl = document.createElement('p');
+			modelEl.textContent = model;
+			modelsEl.append(modelEl);
+		});
+		brandDiv.append(brandEl, modelsEl);
+		outputEl.append(brandDiv);
+	});
+};
+
+document.addEventListener('DOMContentLoaded', async () => {
+	const cars = await onFetch();
+	displayCars(cars);
+});
